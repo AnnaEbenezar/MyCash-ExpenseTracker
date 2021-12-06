6e48a813496e59779984c75cc7d7bdd9cd9e56a3
Hannah Ebenezar <91257701+AnnaEbenezar@users.noreply.github.com>
2021-12-07 04:53:04 +0700

    practicing layouts

---
+++ main.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+
+
+class GridLayoutExample(GridLayout):
+    pass
+
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+
+class MainWidget(Widget):
+    pass
+
+
+class MyCashApp(App):
+    pass
+
+
+MyCashApp().run()
